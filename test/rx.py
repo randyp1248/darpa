320752146dc720be66723a82e9b4e44587e5da3b
Tritons <tritons@UCSD-TRITONS-1.(none)>
2013-05-26 23:29:24 -0700

    Added RRC to rx.py.

--- test/rx.py
+++ test/rx.py
@@ -44,9 +44,23 @@ class my_top_block(gr.top_block):
 
 	# NULL sink block
         self.nullsink = gr.null_sink(gr.sizeof_gr_complex)
+ 
+        # RRC filter
+        nfilts = 32 
+        ntaps = nfilts * 11 * int(options.samples_per_symbol)
+        self.rrc_taps = gr.firdes.root_raised_cosine(
+            nfilts, # gain
+            nfilts, # sampling rate based on 32 fliters in resampler
+            1.0,    # symbol rate
+            1.0,    # excess bandwidth or roll-off factor
+            ntaps)
+        self.rrc_filter = gr.pfb_arb_resampler_ccf(options.samples_per_symbol, 
+                                                   self.rrc_taps)
 
 	# Connect the blocks
-        self.connect(self.source, self.correlator)
+        #self.connect(self.source, self.correlator)
+        self.connect(self.source, self.rrc_filter)
+        self.connect(self.rrc_filter, self.correlator)
         self.connect(self.correlator, self.printer)
         self.connect(self.printer, self.nullsink)
        
